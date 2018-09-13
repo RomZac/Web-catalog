99fcb855d8d5ab558369313a5c9defc55a2caadc
RomZac <parshakov54@yandex.ru>
2018-09-13 11:32:43 +0300

    Update Test_Article.java

--- src/ruunlimit/Test_Article.java
+++ src/ruunlimit/Test_Article.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class Test_Article {
 
 	@Test
-	public void test_parametrs() {
+	public void test_parameters() {
 		Catalog web = new Catalog();
 		web.init();
 		assertEquals("Online", web.web);		
